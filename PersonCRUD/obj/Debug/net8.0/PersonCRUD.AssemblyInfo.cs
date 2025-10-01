eb14b559397e94b3b59ee7ca81ee0ecbcf71ff69
RachelBancoArbi <rachel.silva@bancoarbi.com.br>
2025-10-01 14:55:21 -0300

    adiciona gitignore na pasta certa

--- PersonCRUD/obj/Debug/net8.0/PersonCRUD.AssemblyInfo.cs
+++ PersonCRUD/obj/Debug/net8.0/PersonCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b311321fb07cdd5a3b97dc319832844c486a9f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
